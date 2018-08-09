b9087643a7b87eb40b348abc9001b58a62b63071
Friedrich Doku <friedrichdoku@gmail.com>
2018-08-09 07:21:46 -0500

    Changes to build

--- src/test.sh
+++
@@ -1,5 +0,0 @@
-# Works 
-
-g++ -shared -Wl,-soname=libdems.so -L/usr/local/lib -lssl -lcrypto -ldclxvi -o libdems.so -O3 -fomit-frame-pointer -fPIC -DQHASM conv.cpp ibe.cpp utils.cpp
-g++ test.cpp -L/home/professor/DEMS/crypt/src -ldems -ldclxvi -lcrypto -lssl
-./a.out
